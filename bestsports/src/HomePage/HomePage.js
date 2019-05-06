2fed7aaaab786e6841521b542fd807e9c114af2b
zepedrob16 <ze_pedro_borges@hotmail.com>
2019-05-06 16:04:07 +0100

    Som na homepage

--- bestsports/src/HomePage/HomePage.js
+++ bestsports/src/HomePage/HomePage.js
@@ -1,21 +1,57 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Card, CardBody, CardImg} from "shards-react";
+import { Button, Container, Row, Col, Card, CardBody, CardImg} from "shards-react";
 import { NavLink } from "react-router-dom";
 import './HomePage.css';
-import NavbarComponent from '../Components/NavbarComponent'
+//import NavbarComponent from '../Components/NavbarComponent'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "shards-ui/dist/css/shards.min.css"
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            playingUrl: '',
+            audio: null,
+            playing: false
+        }  
+    }
+
     cardStyle = {
         display: 'block',
         width: '20vw',
         height: '20vw'
     };
 
+    playAudio(previewUrl) {
+        let audio = new Audio(previewUrl);
+        if (!this.state.playing) {
+            audio.play();
+            this.setState({
+                playingUrl: previewUrl,
+                audio,
+                playing: true
+            })
+        }
+        else if (this.state.playing) {
+            this.state.audio.pause();
+            this.setState({
+                playing: null,
+                playingUrl: '',
+                audio: null
+            })
+        }
+    }
+
+    pauseAudio() {
+        this.state.audio.pause();
+    }
+
     render() {
         return (
             <div className="App">
+
+            
             
             <Container>
                 <Row>
@@ -90,6 +126,9 @@ class App extends Component {
                             SC Sporting 5 - 3 Liverpool
                         </Row>
                     </Col>
+                </Row>
+                <Row>
+                    <Button theme="dark" onClick={() => this.playAudio("http://freesound.org/data/previews/191/191929_923993-lq.mp3")}>Play/Stop</Button>
                 </Row>
             </Container>
             </div>
