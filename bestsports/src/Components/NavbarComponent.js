ee1b54a06e38de1ebae4e567abbadf684ef54861
Vicente <up201503764@fe.up.pt>
2019-05-15 11:18:15 +0100

    started creating search component

--- bestsports/src/Components/NavbarComponent.js
+++ bestsports/src/Components/NavbarComponent.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
-import { Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    Button} from "shards-react";
+import { Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, Button } from "shards-react";
 import { NavLink } from "react-router-dom";
+import  Search  from "./Search";
 
 class NavbarComponent extends Component {
     constructor(props) {
         super(props);
-    
+
         this.toggleDropdown = this.toggleDropdown.bind(this);
         this.toggleNavbar = this.toggleNavbar.bind(this);
-    
+
         this.state = {
             dropdownOpen: false,
             collapseOpen: false
@@ -28,7 +24,7 @@ class NavbarComponent extends Component {
             }
         });
     }
-    
+
     toggleNavbar() {
         this.setState({
             ...this.state,
@@ -38,37 +34,40 @@ class NavbarComponent extends Component {
         });
     }
 
-    render(){
+    render() {
         return (
             <Navbar type="dark" theme="dark" sticky fixed expand="md">
                 <NavbarBrand href="#">BestSports</NavbarBrand>
                 <NavbarToggler onClick={this.toggleNavbar} />
 
-            
-                    <Nav navbar className="ml-auto">
-                        <NavItem>
-                            <NavLink to="/">
-                                <Button theme="dark" href="#">
-                                    Home
+                <Nav navbar className="ml-auto">
+                    <Search></Search>
+                </Nav>
+
+                <Nav navbar className="ml-auto">
+                    <NavItem>
+                        <NavLink to="/">
+                            <Button theme="dark" href="#">
+                                Home
                                 </Button>
-                            </NavLink>
-                            
-                        </NavItem>
-                        <NavItem>
-                            <NavLink to="/basketball">
-                                <Button theme="dark" href="#">
-                                    Basketball
+                        </NavLink>
+
+                    </NavItem>
+                    <NavItem>
+                        <NavLink to="/basketball">
+                            <Button theme="dark" href="#">
+                                Basketball
                                 </Button>
-                            </NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink to="/football">
-                                <Button theme="dark" href="#">
-                                    Football
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink to="/football">
+                            <Button theme="dark" href="#">
+                                Football
                                 </Button>
-                            </NavLink>
-                        </NavItem>
-                    </Nav>
+                        </NavLink>
+                    </NavItem>
+                </Nav>
 
             </Navbar>
         );
---
+++ bestsports/src/Components/Search.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import Suggestions from './Suggestions'
+
+class Search extends Component {
+  state = {
+    query: '',
+    results: []
+  }
+
+  getInfo = () => {
+    /*axios.get(`${API_URL}?api_key=${API_KEY}&prefix=${this.state.query}&limit=7`)
+      .then(({ data }) => {
+        this.setState({
+          results: data.data // MusicGraph returns an object named data, 
+                             // as does axios. So... data.data                             
+        })
+      })*/
+  }
+
+  handleInputChange = () => {
+    this.setState({
+      query: this.search.value
+    }, () => {
+      if (this.state.query && this.state.query.length > 1) {
+        if (this.state.query.length % 2 === 0) {
+          this.getInfo()
+        }
+      }
+    })
+  }
+
+  render() {
+    return (
+      <form>
+        <input
+          placeholder="Search for..."
+          ref={input => this.search = input}
+          onChange={this.handleInputChange}
+        />
+        <Suggestions results={this.state.results} />
+      </form>
+    )
+  }
+}
+
+export default Search
+
