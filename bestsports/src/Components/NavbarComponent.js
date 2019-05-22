9f379b89d0bb066bc96daefb43563587070cd5ab
Vicente <up201503764@fe.up.pt>
2019-05-22 12:14:26 +0100

    added animation to title

--- bestsports/src/Components/NavbarComponent.js
+++ bestsports/src/Components/NavbarComponent.js
@@ -3,6 +3,7 @@ import { Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, Button } from "shards
 import { NavLink } from "react-router-dom";
 import Slide from '@material-ui/core/Slide';
 import Search from "./Search";
+import "./navbar.css";
 
 class NavbarComponent extends Component {
     constructor(props) {
@@ -15,8 +16,8 @@ class NavbarComponent extends Component {
             dropdownOpen: false,
             collapseOpen: false,
             gif: null,
-            checked:true,
-            height:null
+            checked: true,
+            height: null
         };
     }
 
@@ -38,7 +39,7 @@ class NavbarComponent extends Component {
         });
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.handlegifs();
     }
 
@@ -46,59 +47,59 @@ class NavbarComponent extends Component {
         if (window.location.href.search('football') !== -1) {
             this.setState({
                 gif: "http://www.animatedimages.org/data/media/165/animated-football-and-soccer-image-0098.gif",
-                height:"60"
+                height: "60"
             });
         } else if (window.location.href.search('basketball') !== -1) {
             this.setState({
                 gif: "https://www.fg-a.com/sports/basketball-slam-dunk-2018.gif",
-                height:"80"
+                height: "80"
             });
         }
     }
 
-    render() {
-        return (
-            <Navbar type="dark" theme="dark" sticky fixed expand="md">
-                <NavbarBrand href="#">BestSports</NavbarBrand>
-                <NavbarToggler onClick={this.toggleNavbar} />
+render() {
+    return (
+        <Navbar type="dark" theme="dark" sticky fixed expand="md">
+            <NavbarBrand href="/">BestSports</NavbarBrand>
+            <NavbarToggler onClick={this.toggleNavbar} />
 
-                <Slide direction="right" in={this.state.checked} >
-                    <img src={this.state.gif} height={this.state.height}></img>
-                </Slide>
+            <Slide direction="right" in={this.state.checked} >
+                <img src={this.state.gif} height={this.state.height}></img>
+            </Slide>
 
-                <Nav navbar className="ml-auto">
-                    <Search></Search>
-                </Nav>
-                
+            <Nav navbar className="ml-auto">
+                <Search></Search>
+            </Nav>
 
-                <Nav navbar className="ml-auto">
-                    <NavItem>
-                        <NavLink to="/">
-                            <Button theme="dark" href="#">
-                                Home
+
+            <Nav navbar className="ml-auto">
+                <NavItem>
+                    <NavLink to="/">
+                        <Button theme="dark" href="#">
+                            Home
                                 </Button>
-                        </NavLink>
+                    </NavLink>
 
-                    </NavItem>
-                    <NavItem>
-                        <NavLink to="/basketball">
-                            <Button theme="dark" href="#">
-                                Basketball
+                </NavItem>
+                <NavItem>
+                    <NavLink to="/basketball">
+                        <Button theme="dark" href="#">
+                            Basketball
                                 </Button>
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink to="/football">
-                            <Button theme="dark" href="#">
-                                Football
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink to="/football">
+                        <Button theme="dark" href="#">
+                            Football
                                 </Button>
-                        </NavLink>
-                    </NavItem>
-                </Nav>
+                    </NavLink>
+                </NavItem>
+            </Nav>
 
-            </Navbar>
-        );
-    }
+        </Navbar>
+    );
+}
 }
 
 export default NavbarComponent;
