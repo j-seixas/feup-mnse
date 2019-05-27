b2b8b46a4667921a4328b403a55c17fa3ab2867a
João Seixas <jnuno1997@gmail.com>
2019-05-27 12:11:53 +0100

    BasketballPage css rework

--- bestsports/src/Basketball/BasketballPage.js
+++ bestsports/src/Basketball/BasketballPage.js
@@ -3,13 +3,13 @@ import { Button, Container, Row, Col, Card, CardBody, CardImg, FormInput, Badge 
 import { NavLink } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "shards-ui/dist/css/shards.min.css"
-
+import "./BasketballPage.css"
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <Container>
-                    <Row>
+                <div className="scoreBar">
+                    <Container>
                         <Col>
                             <h1 className="titleScoreBar">Basketball<br></br>Scores</h1>
                             <Card small className="scoresWrapper">
@@ -65,58 +65,51 @@ class App extends Component {
                                 </CardBody>
                             </Card>
                         </Col>
+                    </Container>
+                </div>
 
-                        <Col>
-                            <Row>
-                                <NavLink to="/basketballnews">
-                                    <Card>
-                                        <CardImg top src="http://bit.ly/2ZZO31G" />
-                                        <CardBody>
-                                            <p>Magic stun No. 2 seed Raptors in final moments of Game 1</p>
-                                        </CardBody>
-                                    </Card>
-                                </NavLink>
-                            </Row>
-                            <Row>
-                                <Card>
-                                    <NavLink to="/bballphilly">
-                                        <CardImg top src="http://bit.ly/2YaKeoE" />
-                                        <CardBody>
-                                            <p>Brooklyn Nets escape with Game 1 win against 76ers.</p>
-                                        </CardBody>
-                                    </NavLink>
-                                </Card>
-                            </Row>
-                        </Col>
-                        <Col>
-                            <Row>
-                                <Button theme="secondary">Primary</Button>
-                                <Button theme="secondary">Secondary</Button>
-                                <Button theme="secondary">Success</Button>
-                            </Row>
-                            <Row>
-                                <NavLink to="/bballhouston">
-                                    <Card>
-                                        <CardImg top src="https://s.hdnux.com/photos/01/02/15/36/17356687/3/920x920.jpg" />
-                                        <CardBody>
-                                            <p>Houston Rockets beat Golden State behind Harden's 38 points</p>
-                                        </CardBody>
-                                    </Card>
-                                </NavLink>
-                            </Row>
-                            <Row>
-                                <NavLink to="/bballceltics">
-                                    <Card>
-                                        <CardImg top src="https://bdn-data.s3.amazonaws.com/uploads/2019/05/28558261_H24763019.JPG" />
-                                        <CardBody>
-                                            <p>Celtics on the verge of getting eliminated by the Bucks.</p>
-                                        </CardBody>
-                                    </Card>
-                                </NavLink>
-                            </Row>
-                        </Col>
-                    </Row>
-                </Container>
+                <div className="news">
+                    <div className="new">
+                        <NavLink to="/basketballnews">
+                            <Card>
+                                <CardImg top src="http://bit.ly/2ZZO31G" />
+                                <CardBody>
+                                    <p>Magic stun No. 2 seed Raptors in final moments of Game 1</p>
+                                </CardBody>
+                            </Card>
+                        </NavLink>
+                    </div>
+                    <div className="new">
+                        <NavLink to="/bballceltics">
+                            <Card>
+                                <CardImg top src="https://bdn-data.s3.amazonaws.com/uploads/2019/05/28558261_H24763019.JPG" />
+                                <CardBody>
+                                    <p>Celtics on the verge of getting eliminated by the Bucks.</p>
+                                </CardBody>
+                            </Card>
+                        </NavLink>
+                    </div>
+                    <div className="new">
+                        <NavLink to="/bballphilly">
+                            <Card>
+                                <CardImg top src="http://bit.ly/2YaKeoE" />
+                                <CardBody>
+                                    <p>Brooklyn Nets escape with Game 1 win against 76ers.</p>
+                                </CardBody>
+                            </Card>
+                        </NavLink>
+                    </div>
+                    <div className="new">
+                        <NavLink to="/bballhouston">
+                            <Card>
+                                <CardImg top src="https://s.hdnux.com/photos/01/02/15/36/17356687/3/920x920.jpg" />
+                                <CardBody>
+                                    <p>Houston Rockets beat Golden State behind Harden's 38 points</p>
+                                </CardBody>
+                            </Card>
+                        </NavLink>
+                    </div>
+                </div>
             </div>
         );
     }
